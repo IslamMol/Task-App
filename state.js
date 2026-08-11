5743c9fcc977ddf7bcc9e15bd8a1aa7dc135aeea
IslamMol <islam20152004@gmail.com>
2026-08-11 16:05:04 +0500

    Delete state.js

--- state.js
+++
@@ -1,9 +0,0 @@
-// Общее состояние приложения. Экспортируется как объект (не отдельные let),
-// чтобы все модули видели одни и те же актуальные значения после мутации.
-export const state = {
-  currentTab: 'quest',
-  session: null,
-  entries: [],
-  compassItems: [],
-  authMode: 'signin',
-};
