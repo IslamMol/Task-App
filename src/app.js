f8b681ae2c258c878f5ea2f5c9894a895fcf26ba
IslamMol <islam20152004@gmail.com>
2026-08-15 00:39:08 +0500

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -3,9 +3,9 @@ import { init, toggleAuthMode, signIn, signUp } from './auth.js';
 import { openCompass, saveCompass, savePathTitle, addJournalEntry } from './compass.js';
 import { toggleDone, toggleHabit, updatePage, deleteEntry, openAdd, saveNew } from './entries.js';
 import { switchMainTab, switchHomeSub, switchTaskSub, initPagerSync, fabClick, toggleStatsView } from './nav.js';
-import { renderDashboard, renderTasksPage, renderFinancePage, openFinanceInfo, openAddForQuest, openHabitsSheet, setActiveAdd, setActiveAdd } from './dashboard.js';
+import { renderDashboard, renderTasksPage, renderFinancePage, openFinanceInfo, openAddForQuest, openHabitsSheet, setActiveAdd } from './dashboard.js';
 import { onSyncStatusChange, syncState } from './db/sync.js';
-import { renderCalendar, renderCalendar, openEventAdd, saveNewEvent, addEventToPhoneCalendar, selectCalendarDate, changeCalendarMonth, setRelativeDay, goToday, selectCalendarDate, changeCalendarMonth, setRelativeDay, goToday } from './pages/calendar.js';
+import { renderCalendar, openEventAdd, saveNewEvent, addEventToPhoneCalendar, selectCalendarDate, changeCalendarMonth, setRelativeDay, goToday } from './pages/calendar.js';
 import { saveAvatar, handleAvatarUpload, usePresetAvatar, getCurrencySymbol, setCurrency, setCustomCurrency, openReorderMenu, setAppLang, changeEmail, changePassword, logout, confirmDeleteAccount, renderSettings } from './pages/settings.js';
 import { moveHomeItem, moveTaskItem, startReorder, finishReorder } from './personalization.js';
 
